c6e99eadd4e9b7e34f97d0cf51e3ce9761b00986
paulosgio <paulosergiodosanjossilvafilho@gmail.com>
2026-08-18 10:55:32 -0300

    fix: add next param in errorHandler

--- src/middlewares/errorHandle.ts
+++ src/middlewares/errorHandle.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { AppError } from "../errors/AppError";
 
-export const errorHandler = (err: any, req: Request, res: Response) => {
+export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
     
     if (err instanceof AppError) {
         return res.status(err.statusCode).json({ message: err.message });
