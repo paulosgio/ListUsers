0d514c6e7bc2a5ec41acd1cc16319e6a13c4be2a
paulosgio <paulosergiodosanjossilvafilho@gmail.com>
2026-08-21 08:49:18 -0300

    fix: passing listId in a response login, change params to params.listId in list.controller, create function to find list by user.id

--- backend/src/modules/list/list.controller.ts
+++ backend/src/modules/list/list.controller.ts
@@ -48,7 +48,7 @@ export class ListController {
 
     public async getListUser(req: Request, res: Response, next: NextFunction) {
         try {
-            const response = await this.listService.getListUserService(Number(req.params))
+            const response = await this.listService.getListUserService(Number(req.params.listId))
             return res.status(200).json(response)
         } catch (error) {
             next(error)
